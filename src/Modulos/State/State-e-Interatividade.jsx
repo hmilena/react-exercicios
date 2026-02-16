f5a72a7dda8e4bd5e3bf912caef278b8091d8d38
Mia <h.milena@gmail.com>
2026-02-16 10:56:44 +0000

    fix: Add `z-10` class to the sticky module title.

--- src/Modulos/State/State-e-Interatividade.jsx
+++ src/Modulos/State/State-e-Interatividade.jsx
@@ -28,7 +28,7 @@ const StateInteratividade = () => {
 
     return (
         <div className="flex flex-col gap-4 bg-white">
-            <div className="ex-title py-4 px-5 sticky top-0 bg-cyan-100 shadow-2xs">
+            <div className="ex-title py-4 px-5 sticky top-0 bg-cyan-100 shadow-2xs z-10">
                 <h1 className="text-2xl font-bold">MÓDULO 2: State e Interatividade (useState, Event Handlers, Forms)</h1>
             </div>
             <div className="ex-content flex flex-col gap-4 px-5 pb-5">
