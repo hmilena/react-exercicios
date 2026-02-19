b887236d255c173fde61437ab54e89512a7830e5
Mia <h.milena@gmail.com>
2026-02-19 20:24:26 +0000

    feat: Implement state management for multiple input fields and display submitted form data.

--- src/Modulos/State/Respostas/MultipleInputForm.jsx
+++ src/Modulos/State/Respostas/MultipleInputForm.jsx
@@ -1,28 +1,58 @@
+import { useState } from "react";
+
 const MultipleInputForm = () => {
+
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        msg: ""
+    })
+
+    const [submittedData, setSubmittedData] = useState(null);
+
+    const handleOnchange = (e) => {
+        const { name, value } = e.target;
+
+        setForm((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setSubmittedData(form);
     }
+
     return (
-        <form className="multiple-input-form inline-flex flex-col al gap-3">
+        <form className="multiple-input-form inline-flex flex-col al gap-3" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="nome">Nome</label>
-                <input type="text" id="nome" />
+                <input type="text" name="name" id="nome" value={form.name} onChange={handleOnchange} />
             </div>
 
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input type="text" name="email" id="email" value={form.email} onChange={handleOnchange} />
             </div>
 
             <div>
                 <label htmlFor="msg">Mensagem</label>
-                <textarea id="msg" />
+                <textarea id="msg" name="msg" value={form.msg} onChange={handleOnchange} />
             </div>
 
             <div>
-                <button className="p-2 bg-blue-500 text-white rounded" onSubmit={handleSubmit}>Enviar</button>
+                <button className="p-2 bg-blue-500 text-white rounded" type="submit">Enviar</button>
             </div>
+
+
+            {submittedData && (
+                <div className="p-3 border rounded border-blue-400">
+                    <p><span className="text-blue-800 italic">Nome:</span> {submittedData.name}</p>
+                    <p><span className="text-blue-800 italic">Email:</span> {submittedData.email}</p>
+                    <p><span className="text-blue-800 italic">Mensagem:</span> {submittedData.msg}</p>
+                </div>
+            )}
         </form>
     )
 }
